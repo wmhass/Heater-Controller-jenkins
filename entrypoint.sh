c464fd81af478c67fe9770aa29e55efc81774bda
William Hass <wm.hass@gmail.com>
2019-10-14 19:33:31 -0300

    disable entrypoint for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-chmod -R 777 /var/run/docker.sock
-cp /var/ssh_keys/id_rsa /var/jenkins_home/.ssh/
-chmod 600 /var/jenkins_home/.ssh/id_rsa
+# chmod -R 777 /var/run/docker.sock
+# sudo cp /var/ssh_keys/id_rsa /var/jenkins_home/.ssh/
+# chmod 600 /var/jenkins_home/.ssh/id_rsa
 
 exec "$@"
