a051f3dc92b69d62e53850a2266262ed618cfd12
William Hass <wm.hass@gmail.com>
2019-10-14 19:30:11 -0300

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-sudo chmod -R 777 /var/run/docker.sock
-sudo cp /var/ssh_keys/id_rsa /var/jenkins_home/.ssh/
+chmod -R 777 /var/run/docker.sock
+cp /var/ssh_keys/id_rsa /var/jenkins_home/.ssh/
 chmod 600 /var/jenkins_home/.ssh/id_rsa
