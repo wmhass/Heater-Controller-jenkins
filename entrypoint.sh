ebb5022d5f8345c34eae7907dd9eb247ffc953e4
William Hass <wm.hass@gmail.com>
2019-10-15 09:37:41 -0300

    creating known hosts

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ echo "========STARTED ENTRY POINT=========="
 chmod -R 777 /var/run/docker.sock
 
 ROOT_HOME_SSH_DIR="/root/.ssh"
-ROOT_HOME_ID_RSA=$JENKINS_HOME_SSH_DIR"/id_rsa"
-ROOT_HOME_ID_RSA_PUB=$JENKINS_HOME_SSH_DIR"/id_rsa.pub"
+ROOT_HOME_ID_RSA=$ROOT_HOME_SSH_DIR"/id_rsa"
+ROOT_HOME_ID_RSA_PUB=$ROOT_HOME_SSH_DIR"/id_rsa.pub"
 
 JENKINS_HOME_SSH_DIR="/var/jenkins_home/.ssh"
 JENKINS_HOME_ID_RSA=$JENKINS_HOME_SSH_DIR"/id_rsa"
@@ -16,9 +16,16 @@ SSH_KEYS_VOLUME="/var/ssh_keys"
 SSH_KEYS_ID_RSA=$SSH_KEYS_VOLUME"/id_rsa"
 SSH_KEYS_ID_RSA_PUB=$SSH_KEYS_VOLUME"/id_rsa.pub"
 
-if [ ! -d $ROOT_HOME_SSH_DIR ]; then mkdir $ROOT_HOME_SSH_DIR; fi
+if [ ! -d $ROOT_HOME_SSH_DIR ]; then
+  mkdir $ROOT_HOME_SSH_DIR;
+  ssh-keyscan github.com >> $ROOT_HOME_SSH_DIR/known_hosts
+fi
 
-if [ ! -d $JENKINS_HOME_SSH_DIR ]; then mkdir $JENKINS_HOME_SSH_DIR; chown jenkins $JENKINS_HOME_SSH_DIR; fi
+if [ ! -d $JENKINS_HOME_SSH_DIR ]; then
+  mkdir $JENKINS_HOME_SSH_DIR;
+  chown jenkins $JENKINS_HOME_SSH_DIR;
+  ssh-keyscan github.com >> $JENKINS_HOME_SSH_DIR/known_hosts
+fi
 
 if [ -d $SSH_KEYS_VOLUME ] ; then
   # Root
@@ -27,6 +34,9 @@ if [ -d $SSH_KEYS_VOLUME ] ; then
     echo "Need to copy ssh key"
     cp $SSH_KEYS_ID_RSA $ROOT_HOME_ID_RSA
     chmod 600 $ROOT_HOME_ID_RSA
+    echo "Root home dir: " $ROOT_HOME_ID_RSA
+    ls -la $ROOT_HOME_ID_RSA
+    ls -la $SSH_KEYS_ID_RSA
   fi
   if [ ! -f $ROOT_HOME_ID_RSA_PUB ] && [ -f $SSH_KEYS_ID_RSA_PUB ]; then
     # id_rsa.pub
